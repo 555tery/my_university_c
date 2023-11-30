6db4be06acb11d93803db123d500c863619214ba
terysol <555tery@gmail.com>
2023-11-30 23:34:54 +0700

    ex 1-2 tour 3

--- C Basic/Tour_12/Ex12_4.c
+++ C Basic/Tour_12/Ex12_4.c
@@ -1,32 +1,36 @@
 //
-// Created by Egor Kapov on 30.10.2023.
+// Created by Egor Kapov on 30.11.2023.
 //
-#include "func.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "math.h"
+#include <stdio.h>
 
-void swap(int *a, int *b) {
+void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
 void shake_sort(int a[], int N) {
-    int left = 0;
-    int right = N-1;
+    int left = 1;
+    int right = N;
     int swapped = 1;
-    while (swapped) {
+    while (swapped)
+    {
         swapped = 0;
-        for (int i = left; i < right; i++) {
-            if (a[i] > a[i + 1]) {
+        for (int i = left; i < right; i++)
+        {
+            if (a[i] > a[i + 1])
+            {
                 swap(&a[i], &a[i + 1]);
                 swapped = 1;
             }
         }
+        swapped = 0;
         right--;
-        for (int i = right; i > left; i--) {
-            if (a[i - 1] > a[i]) {
+
+        for (int i = right; i > left; i--)
+        {
+            if (a[i - 1] > a[i])
+            {
                 swap(&a[i - 1], &a[i]);
                 swapped = 1;
             }
@@ -35,23 +39,20 @@ void shake_sort(int a[], int N) {
     }
 }
 
-int main() {
+int shakeк_sort() {
     int N;
     scanf("%d", &N);
+
     int a[100000];
     for (int i = 0; i < N; i++) {
         scanf("%d", &a[i]);
     }
+
     shake_sort(a, N);
+
     for (int i = 0; i < N; i++) {
         printf("%d ", a[i]);
     }
+
     return 0;
 }
-/*int main() {
-    //alphabet();
-    //difference();
-    //words();
-     //binominal_coefficient();
-    shake_sort();
-}*/
