8f684034a9c5e04b526704c62bfeb87e5ff8f5f9
terysol <555tery@gmail.com>
2023-12-05 03:50:04 +0700

    base

--- C Basic/Tour_7/Ex7_3.c
+++ C Basic/Tour_7/Ex7_3.c
@@ -8,16 +8,25 @@ void code_style() {
     freopen("/Users/555tery/CLionProjects/my_university_c/C Basic/Tour_7/input.txt", "r", stdin);
     freopen("/Users/555tery/CLionProjects/my_university_c/C Basic/Tour_7/output.txt", "w", stdout);
     char symbol;
-    int flag = 1, cnt = 0;
-    while (scanf("%c", &symbol)==1) {
-        if (symbol > 31)
-            flag = 0;
-        if (symbol == 13)
+    int flag = 1, cnt = 0, res = 0;
+    while (scanf("%c", &symbol)!=EOF) {
+
+        if (symbol == '\n') {
             flag = 1;
-        if (flag == 1 && symbol == 32)
+            cnt = 0;
+            //printf("lol\n");
+        }
+        if (symbol > ' ') {
+            res += cnt;
+            flag = 0;
+            cnt=0;
+            //printf("lol2\n");
+
+        }
+        if (flag == 1 && symbol == ' ')
             cnt++;
     }
-    printf("%d", cnt);
+    printf("%d", res);
     fclose(stdout);
     fclose(stdin);
 }
