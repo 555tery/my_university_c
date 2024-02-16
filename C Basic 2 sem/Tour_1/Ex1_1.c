e1a21f829cd64a510b9e4c3c66d9352f71c94a5f
Pokulevskii Danil <555tery@gmail.com>
2024-02-16 18:17:39 +0700

    base

--- C Basic 2 sem/Tour_1/Ex1_1.c
+++ C Basic 2 sem/Tour_1/Ex1_1.c
@@ -13,23 +13,64 @@ typedef struct Res {
 } Res;
 
 int assimpot() {
-    int num_of_test = 0;
-    scanf("%d", &num_of_test);
-    char bufer1[50], bufer2[50];
-    for (int i = 0; i < num_of_test; ++i) {
+    int num_of_tests = 0;
+    scanf("%d\n", &num_of_tests);
+    for (int i = 0; i < num_of_tests; ++i) {
+        Res res1, res2;
+        res1.v_step = 1;
+        res1.v_osn = 0;
+        res1.vlog = 0;
+        res2.v_step = 1;
+        res2.v_osn = 0;
+        res2.vlog = 0;
+        char bufer1[50] = {0};
+        char bufer2[50] = {0};
         gets(bufer1);
         gets(bufer2);
-        char *now = strtok(bufer1, " ");
-        while (now != NULL){
-            sscanf(now, ^N, )
-
+        char *s1, *s2;
+        if (strstr(bufer1, " N ") != NULL)
+            res1.v_osn = 1;
+        if (strstr(bufer2, " N ") != NULL)
+            res2.v_osn = 1;
+        if (strstr(bufer1, " logN ") != NULL)
+            res1.vlog = 1;
+        if (strstr(bufer2, " logN ") != NULL)
+            res2.vlog = 1;
+        s1 = strtok(bufer1, " ");
+        while (s1 != NULL) {
+            sscanf(s1, "logN^%f", &res1.vlog);
+            sscanf(s1, "N^%f", &res1.v_osn);
+            sscanf(s1, "%f^N", &res1.v_step);
+            s1 = strtok(NULL, " ");
         }
+        s1 = strtok(bufer2, " ");
+        while (s1 != NULL) {
+            sscanf(s1, "logN^%f", &res2.vlog);
+            sscanf(s1, "N^%f", &res2.v_osn);
+            sscanf(s1, "%f^N", &res2.v_step);
+            s1 = strtok(NULL, " ");
+        }
+        if (res1.v_step > res2.v_step)
+            printf("1\n");
+        else if (res1.v_step < res2.v_step)
+            printf("-1\n");
+        else if (res1.v_step == res2.v_step) {
+            if (res1.v_osn > res2.v_osn)
+                printf("1\n");
+            else if (res1.v_osn < res2.v_osn)
+                printf("-1\n");
+            else if (res1.v_osn == res2.v_osn) {
+                if (res1.vlog > res2.vlog)
+                    printf("1\n");
+                else if (res1.vlog < res2.vlog)
+                    printf("-1\n");
+                else if (res1.vlog == res2.vlog) {
+                    printf("0\n");
+                }
+            }
+        }
+
     }
 }
-//
-//1
-//O( 1 )
-//O( logN )
 
 
-//O( N^2 )
