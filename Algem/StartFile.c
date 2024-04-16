13fb7032a36c9c7930995f4278b8227ee89707a3
Pokulevskii Danil <555tery@gmail.com>
2024-04-17 00:53:30 +0700

    base

--- Algem/StartFile.c
+++ Algem/StartFile.c
@@ -3,6 +3,7 @@
 #include "stdlib.h"
 #include "iso646.h"
 
+int num_of_jews, every_n_jew;
 typedef struct spisok {
     int value;
     struct spisok *next;
@@ -10,14 +11,13 @@ typedef struct spisok {
 } spisok;
 
 void spisok_builder(spisok *head) {
-    int znachenie = 0;
     spisok *element;
     spisok *tmp;
-    scanf("%d", &head->value);
     head->next = NULL;
     head->prev = NULL;
     tmp = head;
-    while (scanf("%d", &znachenie) != EOF) {
+    head->value = 1;
+    for (int znachenie = 2; znachenie <= num_of_jews; ++znachenie) {
         element = (spisok *) malloc(sizeof(spisok));
         element->value = znachenie;
         element->next = NULL;
@@ -29,18 +29,36 @@ void spisok_builder(spisok *head) {
     head->prev = tmp;
 }
 
+void deleter(spisok *elem) {
+    spisok *tmp = elem;
+    elem = elem->next;
+    elem->prev = tmp->prev;
+    tmp->prev->next = elem;
+    if (elem->next == elem and elem->prev == elem) {
+        elem->prev = NULL;
+        elem->next = NULL;
+    } else free(tmp);
+}
+
 int main() {
     freopen("/Users/555tery/CLionProjects/my_university_c/Algem/input.txt", "r", stdin);
     freopen("/Users/555tery/CLionProjects/my_university_c/Algem/output.txt", "w", stdout);
     spisok *head;
     int cnt = 1;
+    scanf("%d %d", &num_of_jews, &every_n_jew);
     head = (spisok *) malloc(sizeof(spisok));
     spisok *element;
     spisok_builder(head);
-    for (*head; head != NULL and cnt < 40;) {
-        printf("%d ", head->value);
+    for (*head; head->next != NULL;) {
+//        printf("%d ", head->value);
         element = head;
-        head = head->prev;
+        head = head->next;
+        if (cnt % every_n_jew == 0)
+            deleter(head->prev);
+//        element->next = NULL;
         cnt++;
+
     }
+    printf("%d", head->value);
+    free(head);
 }
