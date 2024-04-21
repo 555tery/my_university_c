eca4f7ea6884b350d00950c9fd2b6594cc0da0d4
Pokulevskii Danil <555tery@gmail.com>
2024-04-22 02:23:18 +0700

    base

--- Algem/StartFile.c
+++ Algem/StartFile.c
@@ -1,76 +1,116 @@
 
 #include "stdio.h"
 #include "stdlib.h"
+#define CMP_LT(a, b) ((a) < (b))
+#define CMP_GT(a, b) ((a) > (b))
+
 #include "iso646.h"
 
-int num_of_jews, every_n_jew;
-typedef struct spisok {
-    int value;
-    struct spisok *next;
-    struct spisok *prev;
-} spisok;
+#define CMP_EQ(a, b) ((a) == (b))
 
-spisok *spisok_builder(spisok *head) {
-    spisok *element;
-    spisok *tmp;
-    int znachenie;
-    scanf("%d", &head->value);
-    while (scanf("%d", &znachenie) != EOF) {
-        tmp = head;
-        element = (spisok *) malloc(sizeof(spisok));
-        element->next = NULL;
-        element->prev = NULL;
-        element->value = znachenie;
-        while (1)
-            if (element->value > head->value) {
-                head->prev = element;
-                element->next = head;
-                head = element;
-                break;
-            } else if (tmp->next == NULL) {
-                element->prev = tmp;
-                tmp->next = element;
-                break;
-            }else if (tmp->value >= element->value and tmp->next->value <= element->value  ) {
-                element->next = tmp->next;
-                tmp->next = element;
-                break;
-            }  else tmp = tmp->next;
+typedef struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+    struct node *parent;
+} node;
+
+node *get_free_node(int value, node *parent) {
+    node *tmp = (node *) malloc(sizeof(node));
+    tmp->left = tmp->left = NULL;
+    tmp->data = value;
+    tmp->parent = parent;
+    return tmp;
+}
+
+void insert(node **head, int value) {
+    node *tmp = NULL;
+    if (*head == NULL) {
+        *head = get_free_node(value, NULL);
+        return;
+    }
+    tmp = *head;
+    while (tmp != NULL) {
+        if (CMP_GT(value, tmp->data)) {
+            if (tmp->right != NULL) {
+                tmp = tmp->right;
+                continue;
+            } else {
+                tmp->right = get_free_node(value, tmp);
+                return;
+            }
+        } else if (CMP_LT(value, tmp->data)) {
+            if (tmp->left) {
+                tmp = tmp->left;
+                continue;
+            } else {
+                tmp->left = get_free_node(value, tmp);
+                return;
+            }
+        } else return;
+    }
+}
+
+node *get_min_node(node *root) {
+    while (root->left)
+        root = root->left;
+    return root;
+}
+
+node *get_max_node(node *root) {
+    while (root->right)
+        root = root->right;
+    return root;
+}
+
+void pre_order_traver(node *root) {
+    if (root) {
+        printf("%d ", root->data);
+        pre_order_traver(root->left);
+        pre_order_traver(root->right);
+    }
+}
+
+void post_order_traver(node *root) {
+    if (root!= NULL) {
+        post_order_traver(root->left);
+        post_order_traver(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void in_order_traver(node *root) {
+    if (root) {
+        in_order_traver(root->left);
+        printf("%d ", root->data);
+        in_order_traver(root->right);
 
     }
-    return head;
 }
-
-void deleter(spisok *elem) {
-    spisok *tmp = elem;
-    elem = elem->next;
-    elem->prev = tmp->prev;
-    tmp->prev->next = elem;
-    if (elem->next == elem and elem->prev == elem) {
-        elem->prev = NULL;
-        elem->next = NULL;
-    } else free(tmp);
+void deleteTree(node **root) {
+    if (*root) {
+        deleteTree(&((*root)->left));
+        deleteTree(&((*root)->right));
+        free(*root);
+    }
 }
-
 int main() {
     freopen("/Users/555tery/CLionProjects/my_university_c/Algem/input.txt", "r", stdin);
     freopen("/Users/555tery/CLionProjects/my_university_c/Algem/output.txt", "w", stdout);
-    spisok *head, *tmp;
-    int cnt = 1;
-
-    scanf("%d %d", &num_of_jews, &every_n_jew);
-    head = (spisok *) malloc(sizeof(spisok));
-    head->next = NULL;
-    head->prev = NULL;
-    head = spisok_builder(head);
-    while (1) {
-        printf("%d ", head->value);
-        if (head->next == NULL)
-            break;
-        tmp = head;
-        head = head->next;
-        cnt++;
-        free(tmp);
+    int val = 0;
+    scanf("%d", &val);
+    node *head = get_free_node(val, NULL);
+    while (scanf("%d", &val) != EOF) {
+        insert(&head, val);
     }
-    free(head);
+    node *tmp = head;
+    pre_order_traver(head);
+    printf("\n");
+    post_order_traver(tmp);
+    printf("\n");
+//    in_order_traver(head);
+//    printf("\n");
+    deleteTree(&head);
+    fclose(stdin);
+    fclose(stdout);
 }
